5446f8dca60c00c929cd3452f2b513e2d6e606a6
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-12-28 15:50:30 -0500

    feat: add cube combine maps and checks

--- benches/maps_naive.rs
+++ benches/maps_naive.rs
@@ -1,7 +1,11 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
+use discrete_homology::graph_maps::cube_maps::{combined_cube_maps, CubeMap};
 use discrete_homology::graph_maps::generate_maps_naive;
 use discrete_homology::prelude::*;
+
+const CUBE3_CUBE3_NUM_MAPS: usize = 15488;
+const CUBE3_TO_GSPHERE_NUM_MAPS: usize = 22762;
 
 fn bench_my_cpu_bound(c: &mut Criterion) {
     let mut group = c.benchmark_group("high");
@@ -23,7 +27,7 @@ fn bench_my_cpu_bound(c: &mut Criterion) {
             let cube = CubeGraph::new(n);
             let (maps, num_checked) = generate_maps_naive(&cube, &cube);
             assert!(num_checked == expected_checked); // there are many maps from cube to itself
-            assert!(maps.len() == 15488);
+            assert!(maps.len() == CUBE3_CUBE3_NUM_MAPS as usize);
             // black_box prevents the compiler from optimizing away inputs/outputs
             std::hint::black_box((maps, num_checked));
         })
@@ -44,7 +48,11 @@ fn bench_my_cpu_bound(c: &mut Criterion) {
             let (maps, num_checked) = generate_maps_naive(&cube, &gsphere_graph);
             // print!("num maps: {} num_checked: {}\n", maps.len(), num_checked);
             assert!(num_checked == expected_checked); // there are many maps from cube to itself
-            assert!(maps.len() == 22762, "num maps was {}", maps.len());
+            assert!(
+                maps.len() == CUBE3_TO_GSPHERE_NUM_MAPS,
+                "num maps was {}",
+                maps.len()
+            );
             // black_box prevents the compiler from optimizing away inputs/outputs
             std::hint::black_box((maps, num_checked));
         })
@@ -52,6 +60,50 @@ fn bench_my_cpu_bound(c: &mut Criterion) {
     group.finish();
 
     let mut group_non_constrained = c.benchmark_group("low");
+
+    group_non_constrained.bench_function(BenchmarkId::new("3cube_c5_naive", "1e6"), |b| {
+        b.iter(|| {
+            // time:   [60.776 ms 61.818 ms 63.138 ms]
+            let n = 3;
+            use cube::CubeGraph;
+            use extras::C_N_graph;
+            let cube = CubeGraph::new(n);
+            let c5_graph = C_N_graph(5);
+
+            let expected_checked: u64 = (c5_graph.n() as u64).checked_pow(cube.n()).unwrap();
+            // Assert it fits in usize
+            assert!(expected_checked <= u64::MAX);
+
+            let (maps, num_checked) = generate_maps_naive(&cube, &c5_graph);
+            // print!("num maps: {} num_checked: {}\n", maps.len(), num_checked);
+            assert!(num_checked == expected_checked); // there are many maps from cube to itself
+            assert!(maps.len() == 2475, "num maps was {}", maps.len());
+            // black_box prevents the compiler from optimizing away inputs/outputs
+            std::hint::black_box((maps, num_checked));
+        })
+    });
+
+    group_non_constrained.bench_function(BenchmarkId::new("2cube_c5_naive", "1e6"), |b| {
+        b.iter(|| {
+            // time:   [81.899 µs 81.899 µs 81.89 µs]
+            let n = 2;
+            use cube::CubeGraph;
+            use extras::C_N_graph;
+            let cube = CubeGraph::new(n);
+            let c5_graph = C_N_graph(5);
+
+            let expected_checked: u64 = (c5_graph.n() as u64).checked_pow(cube.n()).unwrap();
+            // Assert it fits in usize
+            assert!(expected_checked <= u64::MAX);
+
+            let (maps, num_checked) = generate_maps_naive(&cube, &c5_graph);
+            // print!("num maps: {} num_checked: {}\n", maps.len(), num_checked);
+            assert!(num_checked == expected_checked); // there are many maps from cube to itself
+            assert!(maps.len() == 95, "num maps was {}", maps.len());
+            // black_box prevents the compiler from optimizing away inputs/outputs
+            std::hint::black_box((maps, num_checked));
+        })
+    });
 
     group_non_constrained.bench_function(BenchmarkId::new("2cube_2cube_naive", "1e6"), |b| {
         b.iter(|| {
@@ -87,11 +139,49 @@ fn bench_my_cpu_bound(c: &mut Criterion) {
             // print!("num maps: {} num_checked: {}\n", maps.len(), num_checked);
             assert!(num_checked == expected_checked); // there are many maps from cube to itself
             assert!(maps.len() == 442, "num maps was {}", maps.len());
+            // 442 / 10^4 = ~4%
             // black_box prevents the compiler from optimizing away inputs/outputs
             std::hint::black_box((maps, num_checked));
         })
     });
+
     group_non_constrained.finish();
+
+    bench_non_naive(c);
+}
+
+fn bench_non_naive(c: &mut Criterion) {
+    let mut group_high = c.benchmark_group("high_non_naive");
+    group_high
+        .sample_size(10) // default is 100
+        .measurement_time(std::time::Duration::from_secs(5))
+        .warm_up_time(std::time::Duration::from_secs(1));
+    group_high.bench_function(BenchmarkId::new("3cube_from_2cube_gsphere", "1e6"), |b| {
+        b.iter(|| {
+            // time:  [7.8974 ms 7.9080 ms 7.9209 ms]
+            let n = 3;
+
+            use cube::CubeGraph;
+            let cube2 = CubeGraph::new(n - 1);
+            let gsphere = extras::greene_sphere();
+            let (cube2_maps, _) = generate_maps_naive(&cube2, &gsphere);
+            let cube2_maps = cube2_maps
+                .into_iter()
+                .map(|m| CubeMap::from(m))
+                .collect::<Vec<_>>();
+            // black_box prevents the compiler from optimizing away inputs/outputs
+            let cube3_maps = combined_cube_maps(&cube2_maps);
+            // O(2475^2) checks = O(~6_000_000) vs 10^8 naive = ~6%
+            assert!(
+                cube3_maps.len() == CUBE3_TO_GSPHERE_NUM_MAPS as usize,
+                "num maps was {}",
+                cube3_maps.len()
+            );
+            std::hint::black_box(cube3_maps);
+        })
+    });
+
+    group_high.finish();
 }
 
 criterion_group!(benches, bench_my_cpu_bound);
--- src/graph_maps/cube_maps.rs
+++ src/graph_maps/cube_maps.rs
@@ -40,7 +40,7 @@ impl<'u, 'v, V: UGraph> CubeMap<'u, 'v, u32, V> {
     pub fn try_combine<'w>(
         &self,
         other: &CubeMap<'w, 'v, u32, V>,
-    ) -> Result<CubeMap<'w, 'v, u32, V>, GraphMapError>
+    ) -> Result<(CubeMap<'w, 'v, u32, V>, CubeMap<'w, 'v, u32, V>), GraphMapError>
     where
         'u: 'w,
     {
@@ -58,14 +58,18 @@ impl<'u, 'v, V: UGraph> CubeMap<'u, 'v, u32, V> {
                 return Err(E::BadEdge(i as u32, i as u32 + n, *x, *y));
             }
         }
+        let combined_len = self.map.vert_maps.len() + other.map.vert_maps.len();
 
-        let mut combined_verts =
-            Vec::with_capacity(self.map.vert_maps.len() + other.map.vert_maps.len());
+        let mut combined_verts = Vec::with_capacity(combined_len);
+        let mut combined_verts_2 = Vec::with_capacity(combined_len);
 
         combined_verts.extend_from_slice(&self.map.vert_maps);
         combined_verts.extend_from_slice(&other.map.vert_maps);
-        let combined = Cow::Owned(CubeGraph::new(n + 1));
+        combined_verts_2.extend_from_slice(&other.map.vert_maps);
+        combined_verts_2.extend_from_slice(&self.map.vert_maps);
 
+        let combined = Cow::Owned(CubeGraph::new(n + 1));
+        let combined_2 = Cow::Owned(CubeGraph::new(n + 1));
         let map = unsafe {
             VertGraphMap::new_unchecked(
                 combined,
@@ -74,10 +78,75 @@ impl<'u, 'v, V: UGraph> CubeMap<'u, 'v, u32, V> {
                 Cow::Owned(combined_verts),
             )
         };
-        Ok(Self { map })
+        let map2 = unsafe {
+            VertGraphMap::new_unchecked(
+                combined_2,
+                // Codomain is almost certainly borrowed so this shouldn't be an issue.:where
+                self.map.codomain.clone(),
+                Cow::Owned(combined_verts_2),
+            )
+        };
+        Ok((Self { map }, Self { map: map2 }))
     }
 
     pub fn d(i: u32, sign: bool) -> CubeMap<'u, 'v, u32, V> {
         panic!()
+    }
+}
+
+pub fn combined_cube_maps<'u, 'v, V: UGraph>(
+    maps: &[CubeMap<'u, 'v, u32, V>],
+) -> Vec<CubeMap<'u, 'v, u32, V>> {
+    let mut combined_maps = Vec::new();
+    let len = maps.len();
+    for i in 0..len {
+        // if let Ok(combined) = maps[i].try_combine(&maps[i]) {
+        //     combined_maps.push(combined.0);
+        //     // combined_maps.push(combined.1);
+        // } else {
+        //     panic!("Could not combine map with itself");
+        // }
+
+        for j in (0..len) {
+            if let Ok(combined) = maps[i].try_combine(&maps[j]) {
+                combined_maps.push(combined.0);
+                // combined_maps.push(combined.1);
+            }
+        }
+    }
+    combined_maps
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::graph_maps::generate_maps_naive;
+
+    use super::*;
+
+    #[test]
+    fn test_2cube_gsphere_combined() {
+        let n = 2;
+
+        use cube::CubeGraph;
+        let source = CubeGraph::new(n);
+        let cube_prev = CubeGraph::new(n - 1);
+        // let target = extras::greene_sphere();
+        let target = extras::C_N_graph(5);
+        let (cube_prev_maps, cube_prev_numchecked) = generate_maps_naive(&cube_prev, &target);
+        let cube_prev_maps = cube_prev_maps
+            .into_iter()
+            .map(|m| CubeMap::from(m))
+            .collect::<Vec<_>>();
+        // black_box prevents the compiler from optimizing away inputs/outputs
+        let cube_n_combined_maps = combined_cube_maps(&cube_prev_maps);
+
+        let cube_n_naive_maps = generate_maps_naive(&source, &target).0;
+
+        assert!(
+            cube_n_combined_maps.len() == cube_n_naive_maps.len(),
+            "num maps combined was {}, but naive was {}",
+            cube_n_combined_maps.len(),
+            cube_n_naive_maps.len()
+        );
     }
 }
